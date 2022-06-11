097a8bca84235cf119eb4b177d6302ea145662ff
Will <will@containerlabs.io>
2022-06-11 11:22:31 -0700

    update module ref

--- src/apis.tf
+++ src/apis.tf
@@ -1,6 +1,6 @@
 
 module "apis" {
-  source = "github.com/massdriver-cloud/terraform-modules//google-enable-apis?ref=9201b9f"
+  source = "github.com/massdriver-cloud/terraform-modules//gcp-enable-apis?ref=c336d59"
   services = [
     # both of these need to be on.... to turn on GCP apis
     # TODO: all a null_resource gcloud command to do it in the module
